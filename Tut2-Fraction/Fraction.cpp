854df8524a4f0d1644602102c8b06b2bee926535
Sanath Maharaj <sanathm7@gmail.com>
2016-02-23 15:13:51 +0200

    initial commit

---
+++ Tut2-Fraction/Fraction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+	Fraction(){
+
+	}
+
+	~Fraction()
+	{
+
+	}
+
+	void setNumDenom()
+	{
+
+	}
+
+	void getNumDenom()
+	{
+
+	}
+
+	Fraction add(Fraction in)
+	{
+
+	}
+
+	Fraction subtract(Fraction in)
+	{
+
+	}
+};
+
+int main()
+{
+
+}
