e346256f551f5fe39067334fe5948ff897e9132d
Sanath Maharaj <sanathm7@gmail.com>
2016-03-01 13:27:41 +0200

    overload operators

--- Tut2-Fraction/Fraction.cpp
+++ Tut2-Fraction/Fraction.cpp
@@ -13,10 +13,10 @@ public:
 	~Fraction();
 	void getNumDenom();
 	void setNumDenom(int num, int denom);
-	Fraction add(Fraction x);
-	Fraction subtract(Fraction x);
-	Fraction multiply(Fraction x);
-	Fraction divide(Fraction x);
+	Fraction operator+(Fraction x);
+	Fraction operator- (Fraction x);
+	Fraction operator* (Fraction x);
+	Fraction operator/ (Fraction x);
 	void print();
 };
 
@@ -39,30 +39,30 @@ void Fraction::setNumDenom(int num, int denom){
 	denominator = denom;
 }
 
-Fraction Fraction::add(Fraction x){
+Fraction Fraction::operator+ (Fraction x){
 	Fraction b = Fraction();
 	b.numerator = (numerator*x.denominator) + (denominator*x.numerator);
 	b.denominator = denominator*x.denominator;
 	return b;
 }
 
-Fraction Fraction::subtract(Fraction x){
+Fraction Fraction::operator- (Fraction x){
 	x.numerator = -x.numerator;
-	return add(x);
+	return operator+ (x);
 }
 
-Fraction Fraction::multiply(Fraction x){
+Fraction Fraction::operator* (Fraction x){
 	Fraction b = Fraction();
 	b.numerator = numerator*x.numerator;
 	b.denominator = denominator*x.denominator;
 	return b;
 }
 
-Fraction Fraction::divide(Fraction x){
+Fraction Fraction::operator/ (Fraction x){
 	int temp = x.numerator;
 	x.numerator = x.denominator;
 	x.denominator = temp;
-	return multiply(x);
+	return operator* (x);
 }
 
 void Fraction::print(){
@@ -95,33 +95,33 @@ int main()
 	Fraction result = Fraction();
 
 	//fraction input
-	a.setNumDenom(5, 4);
+	a.setNumDenom(7, 12);
 	b.setNumDenom(7, 12);
 	
 	//add
 	cout << "Addition:" << endl;
-	result = a.add(b);
+	result = a+b;
 	result.getNumDenom();
 	result.print();
 	cout << endl;
 
-	//subtraction
-	cout << "Subtraction:" << endl;
-	result = a.subtract(b);
+	//operator- ion
+	cout << "operator- ion:" << endl;
+	result = a-b;
 	result.getNumDenom();
 	result.print();
 	cout << endl;
 
 	//multiplication
 	cout << "Multiplication:" << endl;
-	result = a.multiply(b);
+	result = a*b;
 	result.getNumDenom();
 	result.print();
 	cout << endl;
 
 	//division
 	cout << "Division:" << endl;
-	result = a.divide(b);
+	result = a/b;
 	result.getNumDenom();
 	result.print();
 	cout << endl;
